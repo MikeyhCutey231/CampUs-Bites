b832d1ace391464fbf1abd1c66326acaa2299c14
MikeyhCutey231 <153712427+MikeyhCutey231@users.noreply.github.com>
2023-12-20 16:08:15 +0800

    da

--- Customer/functions/myPurchases-filter.php
+++ Customer/functions/myPurchases-filter.php
@@ -79,7 +79,7 @@ function displayOrderDetails($request)
                         echo '<div class="total-title">Order Total:</div>';
                         echo '</div>';
                         echo '<div class="col-lg-1 col-md-2 col-3 d-flex align-items-center justify-content-center">';
-                        echo '<div class="total-con">₱' . number_format($orderTotals[$currentOrderId], 2) . '</div>';
+                        echo '<div class="total-con">₱' . number_format($grandtotal, 2) . '</div>';
                         echo '</div>';
                         echo '</div>';
                         echo '</div>';
